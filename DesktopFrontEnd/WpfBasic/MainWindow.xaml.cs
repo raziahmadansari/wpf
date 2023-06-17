482c664d4acb723c9b49433ae24a032c7466583a
Razi Ahmad Ansari <raziahmad.a@keka.com>
2023-06-18 01:34:11 +0530

    basics of window

--- DesktopFrontEnd/WpfBasic/MainWindow.xaml.cs
+++ DesktopFrontEnd/WpfBasic/MainWindow.xaml.cs
@@ -7,9 +7,14 @@ namespace WpfBasic
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _isDataDirty = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            //this.SizeToContent = SizeToContent.Width;
+            this.ResizeMode = ResizeMode.CanMinimize;
+            this.ShowInTaskbar = false;
         }
 
         private void Add_Name(object sender, RoutedEventArgs e)
@@ -17,8 +22,36 @@ namespace WpfBasic
             if (!string.IsNullOrEmpty(textName.Text) && !listNames.Items.Contains(textName.Text))
             {
                 listNames.Items.Add(textName.Text);
+                MessageBox.Show($"{textName.Text} - is added successfully.");
                 textName.Clear();
             }
+            else
+            {
+                MessageBox.Show($"Can't add {textName.Text}, as it is already added.", "Error!");
+            }
+        }
+
+        private void fileExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            // Close the current window
+            this.Close();
+        }
+
+        private void UserConfirmationOnWindowClose(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this._isDataDirty)
+            {
+                var result = MessageBox.Show("Document has changed, close without saving?", "Question", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void textChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            this._isDataDirty = true;
         }
     }
 }
