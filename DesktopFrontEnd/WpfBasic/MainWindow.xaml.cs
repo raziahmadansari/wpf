5cf4dd8e6b57266a77179a8b915e2eebf6e316b8
Razi Ahmad Ansari <raziahmad.a@keka.com>
2023-06-17 22:24:23 +0530

    added basic wpf project

---
+++ DesktopFrontEnd/WpfBasic/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace WpfBasic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Name(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textName.Text) && !listNames.Items.Contains(textName.Text))
+            {
+                listNames.Items.Add(textName.Text);
+                textName.Clear();
+            }
+        }
+    }
+}
